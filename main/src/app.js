7188a28a9f52d92b74f583836cbc9887ed6574a1
Noman Shaikh <noman.shaikh@neosftmail.com>
2024-01-03 18:32:30 +0530

    Integrating Rabbit MQ

--- main/src/app.js
+++ main/src/app.js
@@ -10,6 +10,42 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var typeorm_1 = require("typeorm");
@@ -17,10 +53,13 @@ var ormconfig_1 = require("../ormconfig");
 var ampq = require("amqplib/callback_api");
 // console.log("ormconf", ormConfig);
 var cors = require("cors");
+var Product_1 = require("./entity/Product");
 var AppDataSource = new typeorm_1.DataSource(__assign({}, ormconfig_1.default));
 AppDataSource.initialize()
-    .then(function () {
+    .then(function (db) {
     console.log("DB CONNECTED");
+    var productRepository = db.getRepository(Product_1.Product);
+    // console.log("productRepository", productRepository);
     ampq.connect("amqps://jngooawy:ezhlKoISJl4g3KdbBVB48-TT1CS-Y6OY@gull.rmq.cloudamqp.com/jngooawy", function (error, connection) {
         if (error) {
             throw new Error(error);
@@ -43,10 +82,99 @@ AppDataSource.initialize()
                 ],
             }));
             app.use(express.json());
-            channel.consume("product_created", function (message) {
-                var eventProduct = JSON.parse(message.content.toString());
-                console.log(message.content.toString(), "message");
-            });
+            channel.consume("product_created", function (message) { return __awaiter(void 0, void 0, void 0, function () {
+                var eventProduct, productData;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            eventProduct = JSON.parse(message.content.toString());
+                            productData = new Product_1.Product();
+                            productData.admin_id = parseInt(eventProduct.id);
+                            productData.title = eventProduct.title;
+                            productData.image = eventProduct.image;
+                            productData.likes = eventProduct.likes;
+                            return [4 /*yield*/, productRepository.save(productData)];
+                        case 1:
+                            _a.sent();
+                            console.log("product created");
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, { noAck: true });
+            channel.consume("product_updated", function (message) { return __awaiter(void 0, void 0, void 0, function () {
+                var eventProduct, product;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            eventProduct = JSON.parse(message.content.toString());
+                            return [4 /*yield*/, productRepository.findOne({
+                                    //@ts-expect-error
+                                    admin_id: parseInt(eventProduct.id),
+                                })];
+                        case 1:
+                            product = _a.sent();
+                            console.log("product", product);
+                            // console.log("eventProduct", eventProduct);
+                            productRepository.merge(product, {
+                                title: eventProduct.title,
+                                image: eventProduct.image,
+                                likes: eventProduct.likes,
+                            });
+                            return [4 /*yield*/, productRepository.save(product)];
+                        case 2:
+                            _a.sent();
+                            console.log("product updated");
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, { noAck: true });
+            channel.consume("product_deleted", function (message) { return __awaiter(void 0, void 0, void 0, function () {
+                var admin_id;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            admin_id = message.content.toString();
+                            // console.log("admin_id", admin_id);
+                            // @ts-expect-error
+                            return [4 /*yield*/, productRepository.delete({ admin_id: admin_id })];
+                        case 1:
+                            // console.log("admin_id", admin_id);
+                            // @ts-expect-error
+                            _a.sent();
+                            console.log("product deleted");
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, { noAck: true });
+            channel.consume("product_liked", function (message) { return __awaiter(void 0, void 0, void 0, function () {
+                var admin_id;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            admin_id = message.content.toString();
+                            // console.log("admin_id", admin_id);
+                            // @ts-expect-error
+                            return [4 /*yield*/, productRepository.delete({ admin_id: admin_id })];
+                        case 1:
+                            // console.log("admin_id", admin_id);
+                            // @ts-expect-error
+                            _a.sent();
+                            console.log("product deleted");
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, { noAck: true });
+            app.get("/api/products", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+                var products;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, productRepository.find()];
+                        case 1:
+                            products = _a.sent();
+                            return [2 /*return*/, res.send(products)];
+                    }
+                });
+            }); });
             app.listen(PORT, function () {
                 console.log("server run on", PORT);
             });
--- main/src/app.ts
+++ main/src/app.ts
@@ -1,22 +1,22 @@
 import { Request, Response, Express } from "express";
 import * as express from "express";
-import { DataSource } from "typeorm";
+import { DataSource, getRepository } from "typeorm";
 import ormConfig from "../ormconfig";
 import * as ampq from "amqplib/callback_api";
 
 // console.log("ormconf", ormConfig);
 
 import * as cors from "cors";
-import { error } from "console";
-import { channel } from "diagnostics_channel";
 import { Product } from "./entity/Product";
 
 const AppDataSource = new DataSource({ ...ormConfig });
 
 AppDataSource.initialize()
-  .then(() => {
+  .then((db) => {
     console.log("DB CONNECTED");
 
+    const productRepository = db.getRepository(Product);
+    // console.log("productRepository", productRepository);
     ampq.connect(
       "amqps://jngooawy:ezhlKoISJl4g3KdbBVB48-TT1CS-Y6OY@gull.rmq.cloudamqp.com/jngooawy",
       (error, connection) => {
@@ -46,13 +46,75 @@ AppDataSource.initialize()
           );
           app.use(express.json());
 
-          channel.consume("product_created", (message) => {
-            const eventProduct: Product = JSON.parse(
-              message.content.toString()
-            );
-            console.log(message.content.toString(), "message");
-          });
+          channel.consume(
+            "product_created",
+            async (message) => {
+              const eventProduct: Product = JSON.parse(
+                message.content.toString()
+              );
+              // console.log("eventProduct", eventProduct);
+              const productData = new Product();
+              productData.admin_id = parseInt(eventProduct.id);
+              productData.title = eventProduct.title;
+              productData.image = eventProduct.image;
+              productData.likes = eventProduct.likes;
+              await productRepository.save(productData);
+              console.log(`product created`);
+            },
+            { noAck: true }
+          );
 
+          channel.consume(
+            "product_updated",
+            async (message) => {
+              const eventProduct: Product = JSON.parse(
+                message.content.toString()
+              );
+              const product = await productRepository.findOne({
+                //@ts-expect-error
+                admin_id: parseInt(eventProduct.id),
+              });
+              console.log("product", product);
+              // console.log("eventProduct", eventProduct);
+              productRepository.merge(product, {
+                title: eventProduct.title,
+                image: eventProduct.image,
+                likes: eventProduct.likes,
+              });
+              await productRepository.save(product);
+              console.log(`product updated`);
+            },
+            { noAck: true }
+          );
+
+          channel.consume(
+            "product_deleted",
+            async (message) => {
+              const admin_id = message.content.toString();
+              // console.log("admin_id", admin_id);
+              // @ts-expect-error
+              await productRepository.delete({ admin_id });
+              console.log(`product deleted`);
+            },
+            { noAck: true }
+          );
+
+          channel.consume(
+            "product_liked",
+            async (message) => {
+              const admin_id = message.content.toString();
+              // console.log("admin_id", admin_id);
+              // @ts-expect-error
+              await productRepository.delete({ admin_id });
+              console.log(`product deleted`);
+            },
+            { noAck: true }
+          );
+
+          app.get("/api/products", async (req: Request, res: Response) => {
+            const products = await productRepository.find();
+            return res.send(products);
+          });
           app.listen(PORT, () => {
             console.log("server run on", PORT);
           });
